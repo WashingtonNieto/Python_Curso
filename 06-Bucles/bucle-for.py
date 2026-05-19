a351f1cb5b314d503aafcdf8c6af040fa8bc55fc
washington <washington_nieto@hotmail.com>
2026-05-19 16:19:53 -0500

    bucles

---
+++ 06-Bucles/bucle-for.py
@@ -0,0 +1,33 @@
+# Bucle for
+
+#for variable in elementos-iterables (lista, arreglo, rango)
+#   bloque de instruccion
+
+# contador = 0
+# resultado = 0
+
+# for contador in range(0,10):
+#     print(f"voy por el numero {str(contador)}")
+#    #resultado = resultado + contador
+#     resultado += contador
+
+# print(f"El resultado es: {resultado}")
+
+# print("_________Ejemplo 2______________")
+# numero_usuario = int(input("Que numero de tabla de multiplicar quieres? "))
+# if numero_usuario <1:
+#     numero_usuario = 1
+
+# print(f"Tabla de multiplicar {numero_usuario}")
+
+# for numero_tabla in range(1,11):
+#     print(f"{numero_usuario} x {numero_tabla} * {numero_usuario*numero_tabla}")
+
+canasta=["mango", "naranja", "banano"]
+
+contador = 0
+for contador in range(0,10):
+    for frutas in canasta:
+        print(frutas)
+
+
