92a4aec478dca36e7813a3b6d60395167c97f294
washington <washington_nieto@hotmail.com>
2026-05-19 12:21:53 -0500

    Condicionales

---
+++ 05-Condicionales/condicionales.py
@@ -0,0 +1,45 @@
+"""
+# Operadores de comparacion
+==  igual
+!=  diferente
+<   Menor que
+>   Mayor que
+<=  Menor igual que
+>=  Mayor igual que
+"""
+
+# condicionales
+# print("########### ejemplo 1 ##########")
+# color = input("Intenta adivinar el color. Cual color eliges: ")
+# if color == "rojo":
+#     print("Bien!! adivinaste el color")
+# else:
+#     print("Mal! intenta de nuevo")
+
+# print("\n########### ejemplo 2 ##########")
+# year=int(input("Entre el año en que estamos: "))
+
+# if year < 2025:
+#     print("Estamos antes del año 2025" )
+# else:
+#     print("Estamos despues del año 2025" )
+
+print("\n########### ejemplo 3 ##########")
+
+nombre = input("Entre su nombre: ")
+ciudad = input("Entre su ciudad: ")
+edad = int(input("entre su edad: "))
+
+mayoriaDeEdad = int(18)
+limiteDeEdadParaTrabajar= int(65)
+
+if edad >= mayoriaDeEdad:
+    print(f"{nombre} es mayor de edad !!")
+    
+    if edad <= limiteDeEdadParaTrabajar:
+        print(f"Tiene edad para trabajar")
+    else:
+        print(f"NO Tiene edad para trabajar")
+    
+else:
+    print(f"{nombre} es menor de edad !!")
