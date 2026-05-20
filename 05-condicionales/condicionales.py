7f41a53976c2706cc1af504c58f721bb2a318130
Washington Nieto <washington_nieto@hotmail.com>
2026-05-19 21:33:54 -0500

    Ejercicio 5

--- 05-condicionales/condicionales.py
+++ 05-condicionales/condicionales.py
@@ -1,10 +1,45 @@
-# Ejemplo 5
-print("\n############ Ejemplo #########")
-edad_minima = 18
-edad_maxima = 65
-edad_oficial =int(input("Que edad tienes?, introduce tu edad: "))
+"""
+# Operadores de comparacion
+==  igual
+!=  diferente
+<   Menor que
+>   Mayor que
+<=  Menor igual que
+>=  Mayor igual que
+"""
 
-if edad_oficial >= 18 and edad_oficial <= 65:
-    print("Esta en edad de trabajar!")
-else: 
-    print("No tienes la para trabajar!")
+# condicionales
+# print("########### ejemplo 1 ##########")
+# color = input("Intenta adivinar el color. Cual color eliges: ")
+# if color == "rojo":
+#     print("Bien!! adivinaste el color")
+# else:
+#     print("Mal! intenta de nuevo")
+
+# print("\n########### ejemplo 2 ##########")
+# year=int(input("Entre el año en que estamos: "))
+
+# if year < 2025:
+#     print("Estamos antes del año 2025" )
+# else:
+#     print("Estamos despues del año 2025" )
+
+print("\n########### ejemplo 3 ##########")
+
+nombre = input("Entre su nombre: ")
+ciudad = input("Entre su ciudad: ")
+edad = int(input("entre su edad: "))
+
+mayoriaDeEdad = int(18)
+limiteDeEdadParaTrabajar= int(65)
+
+if edad >= mayoriaDeEdad:
+    print(f"{nombre} es mayor de edad !!")
+    
+    if edad <= limiteDeEdadParaTrabajar:
+        print(f"Tiene edad para trabajar")
+    else:
+        print(f"NO Tiene edad para trabajar")
+    
+else:
+    print(f"{nombre} es menor de edad !!")
