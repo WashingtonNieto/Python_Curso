77b730f14202e9dd5a068609b485e515dc742614
Washington Nieto <washington_nieto@hotmail.com>
2026-05-19 21:01:48 -0500

    condicionales casa

---
+++ 05-condicionales/condicionales.py
@@ -0,0 +1,10 @@
+# Ejemplo 5
+print("\n############ Ejemplo #########")
+edad_minima = 18
+edad_maxima = 65
+edad_oficial =int(input("Que edad tienes?, introduce tu edad: "))
+
+if edad_oficial >= 18 and edad_oficial <= 65:
+    print("Esta en edad de trabajar!")
+else: 
+    print("No tienes la para trabajar!")
