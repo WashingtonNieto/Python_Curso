3716afefe96c5353698859670651080fb8ed3195
Eileen Rodriguez <erajunio27@gmail.com>
2026-05-21 19:50:08 -0500

    mas while

--- 06-Bucles/bucle-while.py
+++ 06-Bucles/bucle-while.py
@@ -28,6 +28,36 @@ else:
     print("Usted NO esta habilitado para vota")  
 
 
+## Resumen bucle While
 
+# Imprime i mientras i sea menor que 6
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+# Salir del bucle cuando i sea 3
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+
+# Si i es 3, continua con las siguiente iteracion
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+# imprimir un mensaje cuando la condicion sea falsa
+i = 1
+while 1 < 6:
+    print(i)
+    i += 1
+else:
+    print("i no es mas grande que 6")
 
 
