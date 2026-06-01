9854f6ba8ecc2ccd0654d728f496cf810395642a
washington nieto <washington_nieto@hotmail.com>
2026-05-31 20:59:13 -0500

    Actualizacion home

--- 07-Ejercicios/ejercicio10.py
+++ 07-Ejercicios/ejercicio10.py
@@ -1,12 +1,16 @@
-# Ejercicio 10
-# pedir la nota a unos alumnos
-# y sacar por pantalla cuantos han aprobado y cuantos no
+# Ejercicio
+# pedir al usuario la cantidad de alumnos
+# luego pedir la nota de cada alumno
+# y sacar por pantalla cuantos han aprobado ( >=3 ) 
+# y cuantos reprobaron ( <3 )
 
 contador = 1
 aprobados = 0
 reprobados = 0
 
 numero_alumnos = int(input("cuantos alumnos tienes? "))
+
+
 
 while contador <= numero_alumnos:
     nota = int(input(f"Que nota tiene el alumno {contador}? "))
